6b2f2472bd5f78d6048994b136542af7e60c8103
Dennis Stumm <dstumm95@gmail.com>
2015-05-10 10:21:33 +0200

    Bugfix

--- app/src/main/java/de/szut/passkeeper/activity/ListDatabaseActivity.java
+++ app/src/main/java/de/szut/passkeeper/activity/ListDatabaseActivity.java
@@ -136,6 +136,8 @@ public class ListDatabaseActivity extends IRecyclerActivity implements IActivity
         alertDialog.setPositiveButton(R.string.dialog_positive_button_default, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), 0);
                 dialog.dismiss();
                 if (Security.getInstance().checkPassword(editText.getText().toString(), ((DatabaseProperty) vectorUserDatabaseProperties.get(position)).getDatabasePwd())) {
                     Intent intentListCategoryActivity = new Intent(ListDatabaseActivity.this, ListCategoryActivity.class)
