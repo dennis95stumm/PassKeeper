31913b64630c1c31bd750fbd657540416da22093
Sami <al.khatib.sami1990@googlemail.com>
2015-05-22 12:43:26 +0200

    code documentation

--- app/src/main/java/de/szut/passkeeper/utility/GeneratePwdClickListener.java
+++ app/src/main/java/de/szut/passkeeper/utility/GeneratePwdClickListener.java
@@ -12,7 +12,7 @@ public class GeneratePwdClickListener implements View.OnClickListener {
     private EditText editText;
 
     /**
-     * @param editText
+     * @param editText the textfield where the generated password is set
      */
     public GeneratePwdClickListener(EditText editText) {
         this.editText = editText;
@@ -21,6 +21,7 @@ public class GeneratePwdClickListener implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        // set the generated password to the view;
         editText.setText(Security.getInstance().generatePassword());
     }
 }
--- app/src/main/java/de/szut/passkeeper/utility/ViewPwdTouchListener.java
+++ app/src/main/java/de/szut/passkeeper/utility/ViewPwdTouchListener.java
@@ -6,13 +6,13 @@ import android.view.View;
 import android.widget.EditText;
 
 /**
- *
+ * This class implements a custom touchlistener to hide or show a textfields password
  */
 public class ViewPwdTouchListener implements View.OnTouchListener {
     private EditText editText;
 
     /**
-     * @param editText
+     * @param editText the textfield where the password should be shown or hidden
      */
     public ViewPwdTouchListener(EditText editText) {
         this.editText = editText;
@@ -21,10 +21,12 @@ public class ViewPwdTouchListener implements View.OnTouchListener {
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         switch (event.getAction()) {
+            // When the clicked view has been released hide the password
             case MotionEvent.ACTION_UP:
                 editText.setTransformationMethod(new PasswordTransformationMethod());
                 editText.setSelection(editText.length());
                 break;
+            // When the clicked view has been released display the password
             case MotionEvent.ACTION_DOWN:
                 editText.setTransformationMethod(null);
                 editText.setSelection(editText.length());
