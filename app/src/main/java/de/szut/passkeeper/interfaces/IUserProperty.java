6843924cde545c7860c07a50fa49606330994a80
Sami <al.khatib.sami1990@googlemail.com>
2015-05-22 14:00:51 +0200

    comment all things! (2/3)

--- app/src/main/java/de/szut/passkeeper/interfaces/IUserProperty.java
+++ app/src/main/java/de/szut/passkeeper/interfaces/IUserProperty.java
@@ -1,23 +1,24 @@
 package de.szut.passkeeper.interfaces;
 
 /**
- *
+ * This is a generic interface used to create a single recyler-adapter
+ * This interface is used by the classes {@link de.szut.passkeeper.property.DatabaseProperty} {@link de.szut.passkeeper.property.CategoryProperty} {@link de.szut.passkeeper.property.EntryProperty}
  */
 public interface IUserProperty {
     /**
-     * @return
+     * @return the item header for an recyler-item
      */
     String getItemHeader();
 
     /**
      *
-     * @return
+     * @return the item sub-header for an recyler-item
      */
     String getItemSubHeader();
 
     /**
      *
-     * @return
+     * @return the item image for an recyler-item
      */
     int getItemImage();
 }
